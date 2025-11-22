be27428d90e1f44e7fd60b3e3223d2a0368b89d5
Taimoor Hussain <taimoorhussain985@gmail.com>
2025-11-23 02:01:04 +0500

    Add implementation for OpenAI client using Gemini API

---
+++ hello-world/open-ai-through-gemini.py
@@ -0,0 +1,24 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+api_key = os.getenv("GEMINI_API_KEY")
+
+client = OpenAI(
+    api_key=api_key,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai"
+)
+response = client.chat.completions.create(
+    model="gemini-2.5-flash",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {
+            "role": "user",
+            "content": "Explain to me how AI works"
+        }
+    ]
+)
+
+print(response.choices[0].message)
