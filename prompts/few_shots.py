6a89a9a375ff3b8edfcbe29fccc2deb694d0525b
Taimoor Hussain <taimoorhussain985@gmail.com>
2025-11-23 19:02:24 +0500

    feat: Structure more few shots prompting

--- prompts/few_shots.py
+++ prompts/few_shots.py
@@ -2,36 +2,56 @@ from openai import OpenAI
 from dotenv import load_dotenv
 import os
 
+
 load_dotenv()
 
 api_key = os.getenv("GEMINI_API_KEY")
 
+SYSTEM_PROMPT = """
+You are the expert in coding only. You are not allowed to provide information outside of coding.
+If the question is outside coding, respond exactly with:
+'I am sorry, I can only help with coding related questions.'
 
-SYSTEM_PROMPT = """You are the expert in the coding only. You are not allowed to provide information outside of coding. If the question is outside of coding, respond with 'I am sorry, I can only help with coding related questions. And your name is Taimoor Hussain.
+Your name is Taimoor Hussain.
 
+Rules:
+- Strictly follow the output in JSON.
+- If the question is coding related, provide the code in the "code" field and set "isCodingRelated" to true.
+- If the question is not coding related, set the "code" field to null and "isCodingRelated" to false.
 
-Examples: 
+Output format:
+{
+    "code": "string or null",
+    "isCodingRelated": boolean
+}
 
-Q.Can you please give me a a+b whole square ?
-A. Sorry I can't help with that. I am only able to assist with coding related questions.
+Examples:
 
-Q. Can you tell me what is a function in python?
-A. A function in Python is a block of reusable code that performs a specific task. It is defined using the def keyword, followed by the function name and parentheses. Functions help in organizing code, improving readability, and avoiding repetition.
+Q: Can you please give me a a+b whole square ?
+A: {
+    "code": null,
+    "isCodingRelated": false
+}
 
-
-'"""
-
+Q: Can you please give me code of a function that adds two numbers?
+A: {
+    "code": "def add_numbers(a, b):\\n    return a + b",
+    "isCodingRelated": true
+}
+"""
 
 
 client = OpenAI(
     api_key=api_key,
     base_url="https://generativelanguage.googleapis.com/v1beta/openai"
 )
+
+
 response = client.chat.completions.create(
     model="gemini-2.5-flash",
     messages=[
         {"role": "system", "content": SYSTEM_PROMPT},
-        {"role": "user", "content": "Can you please give me translation for 'Hello' in French in python code?"},
+        {"role": "user", "content": "Can you please give a code in js that multiple two numbers?"},
     ]
 )
 
