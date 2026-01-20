8d273473c65c4b334d72eaf6d8c5fdcb64c47ebe
Taimoor Hussain <taimoorhussain985@gmail.com>
2026-01-20 15:04:59 +0500

    feat: Enhance chatbot and sampleNode functions with debug print statements

--- langgraph/chat.py
+++ langgraph/chat.py
@@ -1,7 +1,7 @@
 from typing_extensions import TypedDict
 from typing import Annotated
 from langgraph.graph.message import add_messages
-from langgraph.graph import StateGraph
+from langgraph.graph import StateGraph, START, END
 
 
 class State(TypedDict):
@@ -9,11 +9,13 @@ class State(TypedDict):
 
 
 def chatbot(state: State):
-    return {"messages": ["Hi, This is a message from ChatBot Node"]}
+    print("We are inside the chatbot node\n")
+    return {"messages": ["Hi, This is a message from ChatBot Node\n"]}
 
 
 def sampleNode(state: State):
-    return {"messages": ["Hi, This is a message from Sample Node"]}
+    print("We are inside the sampleNode node\n")
+    return {"messages": ["Hi, This is a message from Sample Node\n"]}
 
 
 graph_bulder = StateGraph(State)
@@ -26,3 +28,18 @@ graph_bulder.add_node(
     "sampleNode",
     sampleNode,
 )
+
+
+graph_bulder.add_edge(START, "chatbot")
+graph_bulder.add_edge("chatbot", "sampleNode")
+graph_bulder.add_edge("sampleNode", END)
+
+
+graph = graph_bulder.compile()
+
+
+updated_state = graph.invoke(
+    State({"messages": ["Hi, This is a message from Initial State\n"]})
+)
+
+print("\n\n Final State:", updated_state)
